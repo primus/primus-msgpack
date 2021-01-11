480a47832901c22bd324b9bee97b30626aab521a
Luigi Pinca <luigipinca@gmail.com>
2021-01-11 09:16:47 +0100

    [pkg] update dev dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 'use strict';
 
 module.exports = {
-  mode: 'production',
+  mode: 'none',
   entry: './browser.js',
   output: {
     libraryTarget: 'commonjs',
     filename: 'bundle.js',
     library: 'msgpack',
     path: __dirname
-  }
+  },
+  target: 'es5'
 };
